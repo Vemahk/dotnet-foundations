8f707983a45517f7a06a730cd5c7016ebc06832b
Vemahk <vemahk20@gmail.com>
2023-05-13 13:55:37 -0500

    reorg-1

--- Vemahk/Kernel/publish.ps1
+++
@@ -1,6 +0,0 @@
-$nuget_out = '.\nuget-out'
-if (Test-Path -LiteralPath $nuget_out) {
-  rm -LiteralPath $nuget_out -Recurse
-}
-dotnet pack .\Main\Vemahk.Kernel.csproj -o .\nuget-out --configuration Release
-dotnet nuget push "${nuget_out}\*.nupkg" --source "github" --skip-duplicate
--- lib/publish-all.ps1
+++ lib/publish-all.ps1
@@ -2,5 +2,10 @@ $nuget_out = '.\nuget-out'
 if (Test-Path -LiteralPath $nuget_out) {
   rm -LiteralPath $nuget_out -Recurse
 }
-dotnet pack .\Main\Vemahk.Infrastructure.Sql.csproj -o .\nuget-out --configuration Release
+
+Get-ChildItem .\*\src\*.csproj -Recurse | % {
+    $proj = $_.FullName;
+    dotnet pack $proj --output .\nuget-out --configuration Release --include-source --include-symbols;
+}
+
 dotnet nuget push "${nuget_out}\*.nupkg" --source "github" --skip-duplicate
