8072303bdf38064a2072621cb1dc0cb8c0aeba8b
Vemahk <vemahk20@gmail.com>
2023-08-13 01:05:04 -0500

    Added polysharp to .netstandard2.0 projects Did some interface cleanup. Trying out VSCode?

---
+++ lib/Vemahk.Infrastructure.Sql/src/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Data.SqlClient;
+using Vemahk.Infrastructure.Interface;
+using Vemahk.Infrastructure.Sql.Connections;
+using Vemahk.Infrastructure.Sql.Interface;
+
+namespace Vemahk.Infrastructure.Sql.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection WithSql(this IServiceCollection services)
+        {
+            services.AddSingleton<ISqlConnectionProvider, SqlConnectionProvider>();
+            services.AddSingleton<IConnectionProvider<SqlConnection>, SqlConnectionProvider>();
+
+            return services;
+        }
+    }
+}
