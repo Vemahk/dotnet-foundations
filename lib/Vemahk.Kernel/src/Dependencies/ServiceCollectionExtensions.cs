c497194b5df2dfc526f940db6fdce58baaeea63d
Vemahk <vemahk20@gmail.com>
2023-11-18 04:49:05 -0600

    add LoadModule

---
+++ lib/Vemahk.Kernel/src/Dependencies/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Vemahk.Kernel.Dependencies;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection LoadModule<T>(this IServiceCollection services) where T : DependencyModule, new()
+    {
+        return new T().Load(services);
+    }
+
+    public static IServiceCollection LoadModule<T>(this IServiceCollection services, IConfiguration config) where T : ConfiguredDependencyModule, new()
+    {
+        return new T().Load(services, config);
+    }
+}
